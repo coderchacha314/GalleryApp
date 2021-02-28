afd7e7d233166e69be3f76dd4a762e5e688e6f22
Abhijeet <abhijeet.bhutkar@capgemini.com>
2021-03-01 00:57:00 +0800

    CURD operation with Firebase API

--- src/app/firebase-api.service.ts
+++ src/app/firebase-api.service.ts
@@ -8,8 +8,17 @@ export class FirebaseAPIService {
   constructor(private http: HttpClient) {}
 
   url = 'https://galleryapp-9acd3-default-rtdb.firebaseio.com/products.json';
+  getProducttitleUrl =
+    'https://galleryapp-9acd3-default-rtdb.firebaseio.com/dataTitle.json';
 
   saveProducts(products: any[]) {
-   return this.http.put(this.url, products);
+    return this.http.put(this.url, products);
+  }
+
+  fetchProducts() {
+    return this.http.get(this.url);
+  }
+  getTitle() {
+    return this.http.get(this.getProducttitleUrl);
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FirebaseAPIService } from '../firebase-api.service';
 
 @Component({
@@ -7,46 +7,73 @@ import { FirebaseAPIService } from '../firebase-api.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private FirebaseAPI:FirebaseAPIService) {}
+  constructor(private FirebaseAPI: FirebaseAPIService) {}
 
-  dataTitle = 'Product List';
-  products = [
-    {
-      id: '1',
-      name: 'Painting',
-      price: '30k',
-    },
-    {
-      id: '2',
-      name: 'Hijab',
-      price: 'RM 35',
-    },
-    {
-      id: '3',
-      name: 'snake Food',
-      price: '10$',
-    },
-  ];
+  dataTitle = this.FirebaseAPI.getTitle();
+  fetchingData = false;
+  @ViewChild('id') id!: ElementRef;
+  @ViewChild('name') name!: ElementRef;
+  @ViewChild('price') price!: ElementRef;
+  products: any = [];
+  editMode = false;
+  editIndex: any;
 
-  onAddProduct(id, name, price) {
-    this.products.push({
-      id: id.value,
-      name: name.value,
-      price: price.value,
-    });
+  onAddProduct(id: any, name: any, price: any) {
+    if (this.editMode) {
+      this.products[this.editIndex] = {
+        id: id.value,
+        name: name.value,
+        price: price.value,
+      };
+      this.editMode = false;
+      this.id.nativeElement.value = '';
+      this.name.nativeElement.value = '';
+      this.price.nativeElement.value = '';
+    } else {
+      this.products.push({
+        id: id.value,
+        name: name.value,
+        price: price.value,
+      });
+    }
   }
 
-  onSaveProduct(){
+  onSaveProduct() {
     this.FirebaseAPI.saveProducts(this.products).subscribe(
-      (Response)=>console.log(Response),
-      (error)=>console.log(error)
-    )
+      (Response) => Response,
+      (error) => console.log(error)
+    );
   }
 
-  onDeleteProduct(id){
-if(confirm("Do You Want to Delete Product?")){
-  this.products.splice(id,1);
-}
+  onFetchProduct() {
+    this.fetchingData = true;
+    this.FirebaseAPI.fetchProducts().subscribe(
+      (Response) => {
+        const data = JSON.stringify(Response);
+        this.products = JSON.parse(data);
+        this.fetchingData = false;
+      },
+      (error) => console.log(error)
+    );
   }
-  ngOnInit(): void {}
+
+  onDeleteProduct(id: any) {
+    if (confirm('Do You Want to Delete Product?')) {
+      this.products.splice(id, 1);
+    }
+    this.onSaveProduct();
+  }
+
+  onEditProduct(index: number) {
+    this.editMode = true;
+    this.editIndex = index;
+    console.log(this.products[index]);
+    this.id.nativeElement.value = this.products[index].id;
+    this.name.nativeElement.value = this.products[index].name;
+    this.price.nativeElement.value = this.products[index].price;
+  }
+
+  ngOnInit(): void {
+    this.onFetchProduct();
+  }
 }
